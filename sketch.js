b0a4bf06007e24ff6731bb481c7b0eaf6ce16a6b
Rob Duarte <me@robduarte.com>
2024-08-28 16:32:00 -0400

    It works with a mouse!

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-  createCanvas(400, 400);
+  //creates a canvas 600 pixels wide
+  //and 400 pixels high
+  createCanvas(600, 400);
 }
-
 function draw() {
-  background(125);
+  //sky blue background
+  background(135, 206, 235);
+  //sun in top-right corner
+  fill("yellow");
+  circle(550, 50, 100);
+ 
+  //grass on bottom half
+  fill("green");
+  rect(0, 200, 600, 200);
+ 
+  //emojis
+  textSize(75)
+  text("🌸", 100, 250) //flower
+  text("🐭", mouseX, mouseY) //ladybug
 }
